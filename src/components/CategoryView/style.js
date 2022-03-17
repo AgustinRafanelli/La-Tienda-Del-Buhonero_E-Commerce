1505c82ed7fc21d81be481380cfa8e1b9ff811ae
Jesus Oyola <jesusoyola@MacBook-Air-de-Jesus.local>
2022-03-17 17:15:41 -0300

    Category View

---
+++ src/components/CategoryView/style.js
@@ -0,0 +1,33 @@
+import styled from 'styled-components/macro';
+
+export const StyledContainer = styled.div`
+
+.title{
+    text-align: left;
+    margin: 40px 0;
+    margin-left:40px ;
+
+}
+.products{
+    display: flex;
+flex-wrap: wrap;
+width: 95%;
+justify-content: center;
+margin-left: 20px;
+
+
+.product{
+    height: 500px;
+    width: 290px;
+      flex-direction: column;
+       align-items: space-between;
+        justify-content: left;
+        margin: 10px;
+            
+}
+
+}
+
+
+  
+`;
