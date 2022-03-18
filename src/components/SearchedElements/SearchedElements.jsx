edbed0aba4a6ff747d67231fd63918bbcc9d6691
juan41271 <jpablozicato2020@gmail.com>
2022-03-18 12:42:19 -0300

    URI encoded added to de URL

--- src/components/SearchedElements/SearchedElements.jsx
+++ src/components/SearchedElements/SearchedElements.jsx
@@ -1,8 +1,8 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { StyledContainer } from "./style";
-import GridCard from "../../commons/GridProducts/GridCard";
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { StyledContainer } from './style';
+import GridCard from '../../commons/GridProducts/GridCard';
 
 export default function SearchedElements() {
   const [data, setData] = useState([]);
@@ -10,20 +10,23 @@ export default function SearchedElements() {
 
   useEffect(() => {
     return axios
-      .get(`/api/products/search/${title}`)
-      .then((res) => setData(res.data));
+      .get(`/api/products/search/${decodeURI(title)}`)
+      .then(res => setData(res.data));
   }, [title]);
   return (
     <StyledContainer>
-      <h1 className="title"> Results for your search: {title} </h1>
-      <div className="products">
-        {" "}
-        {data.length ?
-          data.map((product) => (
-            <div className="product" key={product.id}>
-              <GridCard product={product}  />
+      <h1 className='title'> Results for your search: {title} </h1>
+      <div className='products'>
+        {' '}
+        {data.length ? (
+          data.map(product => (
+            <div className='product' key={product.id}>
+              <GridCard product={product} />
             </div>
-          )): <h1>Product Not Found</h1>}
+          ))
+        ) : (
+          <h1>Product Not Found</h1>
+        )}
       </div>
     </StyledContainer>
   );
